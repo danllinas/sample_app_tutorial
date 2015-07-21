57e501307a6e65faead32de3b390cc548f8a22f3
Daniel Llinas <danllinas@gmail.com>
2015-07-20 23:18:32 -0400

    Improved regex for email validation.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -39,8 +39,8 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "validation should reject invalid email addresses" do
-    invalid_addreses = %w(user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com)
-    invalid_addreses.each do |invalid_address|
+    invalid_addresses = %w(user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com)
+    invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid? "#{invalid_address.inspect} should be invalid."
     end
