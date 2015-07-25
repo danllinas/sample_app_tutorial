390697ee7b209f98544147cf62f931a2b7fb3761
Daniel Llinas <danllinas@gmail.com>
2015-07-25 00:31:24 -0400

    Finish login/log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include SessionsHelper #makes all the functions in the helper available across all the controllers int he application.
 
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,8 +10,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar"}
     end
      assert_template 'users/new'
-     assert_select 'div#<CSS id for error explanation>'
-     assert_select 'div.<CSS class for field with error>'
   end
 
   test "valid signup info" do
@@ -24,6 +22,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/show'
     assert_not flash.nil?
+    assert is_logged_in?
   end
 
 end
